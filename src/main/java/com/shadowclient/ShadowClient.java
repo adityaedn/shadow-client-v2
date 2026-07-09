49b80ad371700bd30f5c83bc74b40c46c807ce1e
adityaedn <adityaraj789088@gmail.com>
2026-07-10 00:29:26 +0530

    Update ShadowClient.java

--- src/main/java/com/shadowclient/ShadowClient.java
+++ src/main/java/com/shadowclient/ShadowClient.java
@@ -1,6 +1,5 @@
 package com.shadowclient;
 
-import com.shadowclient.module.ModuleManager;
 import com.shadowclient.screen.ShadowModulesScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -8,32 +7,29 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 
-public final class ShadowClient implements ClientModInitializer {
+public class ShadowClient implements ClientModInitializer {
     public static final String MOD_ID = "shadowclient";
-    public static final Text NAME = Text.literal("Shadow Client");
 
-    private static KeyBinding openMenuKey;
+    public static KeyBinding OPEN_MENU_KEY;
+
+    public static final KeyBinding.Category SHADOW_CATEGORY =
+            KeyBinding.Category.create(Identifier.of(MOD_ID, "category"));
 
     @Override
     public void onInitializeClient() {
-        ModuleManager.init();
-
-        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        OPEN_MENU_KEY = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.shadowclient.open_menu",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_RIGHT_SHIFT,
-                "key.category.shadowclient"
+                SHADOW_CATEGORY
         ));
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (openMenuKey.wasPressed()) {
-                MinecraftClient mc = MinecraftClient.getInstance();
-                if (!(mc.currentScreen instanceof ShadowModulesScreen)) {
-                    mc.setScreen(new ShadowModulesScreen());
-                }
+            while (OPEN_MENU_KEY.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(new ShadowModulesScreen());
             }
         });
     }
